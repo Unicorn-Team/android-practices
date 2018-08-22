cb1ebd1da9d0d1fe1878dacd6c953bde8bc6e5fa
skliu <skliu@Ctrip.com>
2018-08-22 22:22:25 +0800

    lsk：首页完成

--- lsk/ExamOne/app/src/main/java/com/example/liushukun/examone/Adapter/Entrance/Main/CardListAdapter.java
+++ lsk/ExamOne/app/src/main/java/com/example/liushukun/examone/Adapter/Entrance/Main/CardListAdapter.java
@@ -11,19 +11,34 @@ import com.example.liushukun.examone.R;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class CardListAdapter extends RecyclerView.Adapter<CardListAdapter.MyVH>{
+public class CardListAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder>{
     private ArrayList<HashMap<String, String>> list;
+    private static final int VH_FIRST_CARD = 0;
+    private static final int VH_NROMAL_CARD = 1;
+    private static final int VH_CLOSE_CARD = -1;
+
     public CardListAdapter(ArrayList<HashMap<String, String>> data) {
         list = data;
     }
 
     @Override
-    public void onBindViewHolder(MyVH holder, int position) {
-        holder.head.setText(list.get(position).get("head"));
-        holder.title.setText(list.get(position).get("title"));
-        holder.author.setText(list.get(position).get("author"));
-        holder.content.setText(list.get(position).get("content"));
-        holder.time.setText(list.get(position).get("time"));
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        switch (getItemViewType(position)) {
+            case VH_FIRST_CARD:
+                FirstVH firstVH = (FirstVH) holder;
+                firstVH.photo.setText(list.get(position).get("photo"));
+                firstVH.slogan.setText(list.get(position).get("slogan"));
+                firstVH.author.setText(list.get(position).get("author"));
+                break;
+            default:
+                NormalVH normalVH = (NormalVH) holder;
+                normalVH.head.setText(list.get(position).get("head"));
+                normalVH.title.setText(list.get(position).get("title"));
+                normalVH.author.setText(list.get(position).get("author"));
+                normalVH.content.setText(list.get(position).get("content"));
+                normalVH.time.setText(list.get(position).get("time"));
+                break;
+        }
     }
 
     @Override
@@ -32,25 +47,51 @@ public class CardListAdapter extends RecyclerView.Adapter<CardListAdapter.MyVH>{
     }
 
     @Override
-    public MyVH onCreateViewHolder(ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.entrance_main_normal_card, parent,false);
-        return new MyVH(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        RecyclerView.ViewHolder myVH;
+        switch (viewType) {
+            case VH_FIRST_CARD:
+                myVH = new FirstVH(LayoutInflater.from(parent.getContext()).inflate(R.layout.entrance_main_first_card, parent,false));
+                break;
+            default:
+                myVH = new NormalVH(LayoutInflater.from(parent.getContext()).inflate(R.layout.entrance_main_normal_card, parent,false));
+                break;
+        }
+        return myVH;
     }
 
-    public static class MyVH extends RecyclerView.ViewHolder{
+    @Override
+    public int getItemViewType(int position){
+        return new Integer(list.get(position).get("type"));
+    }
+
+    public static class NormalVH extends RecyclerView.ViewHolder{
         public final TextView head;
         public final TextView title;
         public final TextView author;
         public final TextView content;
         public final TextView time;
 
-        public MyVH(View v) {
+        public NormalVH(View v) {
             super(v);
             head = (TextView) v.findViewById(R.id.entranceMainCardHead);
             title = (TextView) v.findViewById(R.id.entranceMainCardTitle);
             author = (TextView) v.findViewById(R.id.entranceMainCardAuthor);
             content = (TextView) v.findViewById(R.id.entranceMainCardContent);
             time = (TextView) v.findViewById(R.id.entranceMainCardTime);
+        }
+    }
+
+    public static class FirstVH extends RecyclerView.ViewHolder{
+        public final TextView photo;
+        public final TextView slogan;
+        public final TextView author;
+
+        public FirstVH(View v) {
+            super(v);
+            photo = (TextView) v.findViewById(R.id.testViewPhoto);
+            slogan = (TextView) v.findViewById(R.id.textViewSlogan);
+            author = (TextView) v.findViewById(R.id.textViewAuthor);
         }
     }
 }
--- lsk/ExamOne/app/src/main/java/com/example/liushukun/examone/Adapter/Entrance/Main/CardListAdapterModel.java
+++ lsk/ExamOne/app/src/main/java/com/example/liushukun/examone/Adapter/Entrance/Main/CardListAdapterModel.java
@@ -13,13 +13,21 @@ public class CardListAdapterModel {
 
     private void mockList() {
         HashMap<String, String> map = new HashMap<String, String>();
+        map.put("type", "0");
+        map.put("photo", "Photo | Liu Yanzu");
+        map.put("slogan", "Legends never die! This world need more heroes!");
+        map.put("author", "Lu Xun");
+        list.add(0, map);
+
+        map = new HashMap<String, String>();
+        map.put("type", "1");
         map.put("head", "- ONE STORY -");
         map.put("title", "第三人称");
         map.put("author", "词 / 刘书昆");
         map.put("content", "她想知道那是谁，为何总沉默寡言，人群中也算抢眼，抢眼的孤独难免。快乐当然多一点，也许寂寞更强烈，难过时候不流泪，流泪也不算伤悲。天真以为是他的独特品味，殊不知是他，难以言喻的对决。");
         map.put("time", "今天");
 
-        list.add(0, map);
+        list.add(1, map);
     }
 
     public ArrayList<HashMap<String, String>> getList() {
